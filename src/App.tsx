e63f35674836d0437aec1a31b0e9c75ae7167890
yunas <ganifbros@gmail.com>
2022-12-24 19:02:19 +0500

    Made the Site and Essay ratings collection functional

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,49 @@ const starRatingsUpdatedHandler = (value: number) => {
 }
 
 export default function App() {
+  const [shouldShowStarsModal, setShouldShowStarsModal] = React.useState(false);
+  const [essayFeedback, setEssayFeedback] = React.useState(0);
+  const [siteFeedback, setSiteFeedback] = React.useState(0);
+  const essayFeedbackTitle: string = 'Please provide your feedback on auto-generated Essay';
+  const siteFeedbackTitle: string = 'How satisfied are you with Smodin services?';
+  const [alertTitle, setAlertTitle] = React.useState(essayFeedbackTitle);
+
+  const showStarsModal = () => {
+    setTimeout(() => {
+      setShouldShowStarsModal(true);
+    }, 700);
+   }
+  
+    React.useEffect(() => {
+      showStarsModal();
+    }, []);
+
+    const starRatingsUpdatedHandler = (value: number) => {
+      if (value > 0){
+        if (essayFeedback == 0){
+          setEssayFeedback(value);
+          console.log('essayFeedback => ', essayFeedback);
+  
+          setAlertTitle(siteFeedbackTitle);
+          setShouldShowStarsModal(false);
+          showStarsModal();
+        }
+        else{
+          setSiteFeedback(value);
+          setShouldShowStarsModal(false);
+        }
+      }
+    };
+  
+    const trustPilotHandler = () => {
+        console.log('Please hit the api here');
+    }
+
+
   return (
     <Container maxWidth="sm">
       <EssayScreen />
-      <BasicModal title={`Title`} callback={starRatingsUpdatedHandler}/>
+      {shouldShowStarsModal && <BasicModal title={alertTitle} callback={starRatingsUpdatedHandler} />}
 
     </Container>
   );
